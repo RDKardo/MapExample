29332e4498ecef9b9df6da9db47f014a1e03da7a
Ricardo <rdkardo@gmail.com>
2021-07-26 20:44:38 -0500

    Quitando Componente mapa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-import { MapaComponent } from './mapa/mapa.component';
 import { AgmCoreModule } from '@agm/core';
 
 
 @NgModule({
   declarations: [
-    AppComponent,MapaComponent
-    // AgmCoreModule.forRoot({ apiKey: 'AIzaSyAe43Dr1LYNEBhj1BWGosOSBpD'}) 
   ],
   imports: [
     
     BrowserModule,
     RouterModule ,
-    AppRoutingModule,
-    AgmCoreModule.forRoot({ apiKey:'AIzaSyAe43Dr1LYNEBhj1BWGosOSBpD-RiVtzQo'})
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
